a43f003f16f08750d4426ec4bb241a1b31102876
guihi1 <gui.hikio@gmail.com>
2022-09-02 16:22:37 -0300

    Add conditions to event listeners

--- index.js
+++ index.js
@@ -61,7 +61,8 @@ const del = document.querySelector(".del");
 const equal = document.querySelector(".equal");
 const display = document.querySelector(".display");
 const displayCalc = document.querySelector(".display-results");
-let op;
+const warning = document.querySelector(".warnings");
+let op = null;
 let pastNumber;
 let currentNumber;
 let answer;
@@ -110,71 +111,161 @@ n9.addEventListener("click", () => {
 clear.addEventListener("click", () => {
     display.textContent = "";
     displayCalc.textContent = "";
+    op = null;
+    pastNumber = null;
+    currentNumber = null;
+    removeWarning();
+    enableDot();
 });
 
 x.addEventListener("click", () => {
-    pastNumber = display.textContent;
-    display.textContent += x.textContent;
-    displayCalc.textContent = display.textContent;
-    display.textContent = "";
-    op = "*";
+    if(op !== null){
+        currentNumber = Number(display.textContent);
+        display.textContent += x.textContent;
+        displayCalc.textContent += display.textContent;
+        display.textContent = "";
+        pastNumber = operator(op, pastNumber, currentNumber);
+        op = "*";
+    } else {
+        pastNumber = Number(display.textContent);
+        display.textContent += x.textContent;
+        displayCalc.textContent = display.textContent;
+        display.textContent = "";
+        op = "*";
+    }
+    removeWarning();
+    enableDot();
 });
 
 pow.addEventListener("click", () => {
-    pastNumber = display.textContent;
-    display.textContent += "^";
-    displayCalc.textContent = display.textContent;
-    display.textContent = "";
-    op = "^"
+    if(op !== null){
+        currentNumber = Number(display.textContent);
+        display.textContent += pow.textContent;
+        displayCalc.textContent += display.textContent;
+        display.textContent = "";
+        pastNumber = operator(op, pastNumber, currentNumber);
+        op = "^";
+    } else {
+        pastNumber = Number(display.textContent);
+        display.textContent += "^";
+        displayCalc.textContent = display.textContent;
+        display.textContent = "";
+        op = "^"
+    }
+    removeWarning();
+    enableDot();
 });
 
 div.addEventListener("click", () => {
-    pastNumber = display.textContent;
-    display.textContent += div.textContent;
-    displayCalc.textContent = display.textContent;
-    display.textContent = "";
-    op = "/";
+    if(op !== null){
+        currentNumber = Number(display.textContent);
+        display.textContent += div.textContent;
+        displayCalc.textContent += display.textContent;
+        display.textContent = "";
+        pastNumber = operator(op, pastNumber, currentNumber);
+        op = "/";
+    } else {
+        pastNumber = Number(display.textContent);
+        display.textContent += div.textContent;
+        displayCalc.textContent = display.textContent;
+        display.textContent = "";
+        op = "/";
+    }
+    removeWarning();
+    enableDot();
 });
 
 dot.addEventListener("click", () => {
-    display.textContent += dot.textContent;
+    if(display.textContent.indexOf(".") !== -1) {
+        dot.setAttribute("disabled", true);
+        warning.textContent = "You can only use one dot per number!"
+    } else {
+        display.textContent += dot.textContent;
+    }
 });
 
 minus.addEventListener("click", () => {
-    pastNumber = display.textContent;
-    display.textContent += minus.textContent;
-    displayCalc.textContent = display.textContent;
-    display.textContent = "";
-    op = "-";
+    if(op !== null){
+        currentNumber = Number(display.textContent);
+        display.textContent += minus.textContent;
+        displayCalc.textContent += display.textContent;
+        display.textContent = "";
+        pastNumber = operator(op, pastNumber, currentNumber);
+        op = "-";
+    } else {
+        pastNumber = Number(display.textContent);
+        display.textContent += minus.textContent;
+        displayCalc.textContent = display.textContent;
+        display.textContent = "";
+        op = "-";
+    }
+    removeWarning();
+    enableDot();
 });
 
 plus.addEventListener("click", () => {
-    pastNumber = display.textContent;
-    display.textContent += plus.textContent;
-    displayCalc.textContent = display.textContent;
-    display.textContent = "";
-    op = "+";
+    if(op !== null){
+        currentNumber = Number(display.textContent);
+        display.textContent += plus.textContent;
+        displayCalc.textContent += display.textContent;
+        display.textContent = "";
+        pastNumber = operator(op, pastNumber, currentNumber);
+        op = "+";
+    } else {
+        pastNumber = Number(display.textContent);
+        display.textContent += plus.textContent;
+        displayCalc.textContent = display.textContent;
+        display.textContent = "";
+        op = "+";
+    }
+    removeWarning();
+    enableDot();
 });
 
 root.addEventListener("click", () => {
-    pastNumber = display.textContent;
+    if(op !== null){
+        currentNumber = Number(display.textContent);
+        display.textContent += root.textContent;
+        displayCalc.textContent += display.textContent;
+        display.textContent = "";
+        pastNumber = operator(op, pastNumber, currentNumber);
+        op = "root";
+    } else {
+    pastNumber = Number(display.textContent);
     display.textContent += root.textContent;
     displayCalc.textContent = display.textContent;
     display.textContent = "";
     op = "root";
+    }
+    removeWarning();
+    enableDot();
 });
 
 del.addEventListener("click", () => {
     display.textContent = display.textContent.slice(0, -1);
+    removeWarning();
 });
 
 equal.addEventListener("click", () => {
-    currentNumber = display.textContent;
-    displayCalc.textContent += currentNumber;
-    displayCalc.textContent += equal.textContent;
-    display.textContent = operator(op, pastNumber, currentNumber);
-    answer = operator(op, pastNumber, currentNumber);
-    pastNumber = currentNumber;
-    currentNumber = answer;
-    op = null;
+    if(pastNumber === undefined){
+        warning.textContent = "Select an operator first";
+    } else {
+        currentNumber = Number(display.textContent);
+        displayCalc.textContent += currentNumber;
+        displayCalc.textContent += equal.textContent;
+        display.textContent = operator(op, pastNumber, currentNumber);
+        answer = operator(op, pastNumber, currentNumber);
+        currentNumber = answer;
+        pastNumber = null;
+        op = null;
+    }
+    enableDot();
 });
+
+function enableDot() {
+    document.querySelector(".dot").disabled = false;
+}
+
+function removeWarning() {
+    warning.textContent = "";
+}
